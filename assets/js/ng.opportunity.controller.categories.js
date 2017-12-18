9f4162c21ef387de19a2b15b2be5a21d7b1ee266
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-12-18 19:11:10 -0200

    Adição de campos select no formulário de inscrição no plugin avalição Sefic

--- assets/js/ng.opportunity.controller.categories.js
+++ assets/js/ng.opportunity.controller.categories.js
@@ -5,9 +5,10 @@
     
     module.controller('CategoriesController',['$scope', 'EditBox', function($scope, EditBox){
         $scope.editBox = EditBox;
-        console.log(MapasCulturais.segmentos);
+        // console.log(MapasCulturais.segmentos);
         
         $scope.segmentos = MapasCulturais.segmentos;
+        
 
         // var n1 = MapasCulturais.entity.tipologia_nivel1;
         // var n2 = MapasCulturais.entity.tipologia_nivel2;
@@ -18,9 +19,10 @@
         //     types[val].__values = Object.keys(types[val]);
         // });
         
+
         $scope.data = {
-            // _tipo1: n1,
-            // _tipo2: n2,
+            // _tipo1: [],
+            // _tipo2: [],
             // _tipo3: n3,
             
             // tipologia1: n1,
@@ -31,46 +33,40 @@
             // _valores_nivel1: types.__values,
             // _valores_nivel2: n1 ? types[n1].__values : [],
             // _valores_nivel3: n2 ? types[n1][n2] : [],
+            _areas: $scope.segmentos,
+            _segmentos: []
         };
+        
+
         
         $scope.set = function(n){
             if(n === 1){
-                $scope.data._valores_nivel2 = types[$scope.data._tipo1].__values;
-                $scope.data._valores_nivel3 = [];
+                $scope.data._segmentos = $scope.data._areas[$scope.data._tipo1];
                 
                 $scope.data._tipo2 = '';
-                $scope.data._tipo3 = '';
-                
-            } else if (n === 2) {
-                $scope.data._valores_nivel3 = types[$scope.data._tipo1][$scope.data._tipo2];
-                $scope.data._tipo3 = '';
             }
         };
         
         var setEditables = function(){
-            $('#tipologia_nivel1').first().editable('setValue', $scope.data.tipologia1);
-            $('#tipologia_nivel2').first().editable('setValue', $scope.data.tipologia2);
-            $('#tipologia_nivel3').first().editable('setValue', $scope.data.tipologia3);  
+            $('#area').first().editable('setValue', $scope.data.area);
+            $('#segmento').first().editable('setValue', $scope.data.segmento);
         };
         
         setEditables();
         
         $scope.setTypes = function(){
-            $scope.data.tipologia1 = $scope.data._tipo1;
-            $scope.data.tipologia2 = $scope.data._tipo2;
-            $scope.data.tipologia3 = $scope.data._tipo3;
+            $scope.data.area = $scope.data._tipo1;
+            $scope.data.segmento = $scope.data._tipo2;
             
             setEditables();
             
-            $scope.data.tipologia = $scope.data._tipo3;
             EditBox.close('eb-tipologia');
         };
         
         
         $scope.resetValues = function(){
-            $scope.data._tipo1 = $scope.data.tipologia1;
-            $scope.data._tipo2 = $scope.data.tipologia2;
-            $scope.data._tipo3 = $scope.data.tipologia3;
+            $scope.data._tipo1 = $scope.data.area;
+            $scope.data._tipo2 = $scope.data.segmento;
         };
         
     }]);
--- layouts/parts/singles/registration-edit--categories.php
+++ layouts/parts/singles/registration-edit--categories.php
@@ -1,6 +1,6 @@
-<?php if($opportunity->registrationCategories): ?>
+
+<!--<?php if($opportunity->registrationCategories): ?>
     <div class="registration-fieldset">
-        <!-- selecionar categoria -->
         <h4><?php echo $opportunity->registrationCategTitle ?></h4>
         <p class="registration-help"><?php echo $opportunity->registrationCategDescription ?></p>
         <p>
@@ -8,3 +8,27 @@
         </p>
     </div>
 <?php endif; ?>
+-->
+
+
+<div class="registration-fieldset" ng-controller="CategoriesController">
+    <h4>Tipologia de categoria</h4>
+    <a class="required editable" ng-click="editBox.open('eb-tipologia', $event)"> {{data.segmento ? data.segmento : 'Escolha um segmento'}}</a>
+
+    <edit-box id="eb-tipologia" position="bottom" cancel-label="Cancelar" submit-label="Enviar" on-submit="setTypes" on-cancel="resetValues" close-on-cancel="1">
+        <input type="hidden" id="data.area" class="js-editable" data-edit="data.area" data-emptytext="">
+        <input type="hidden" id="data.segmento" class="js-editable" data-edit="data.segmento" data-emptytext="">
+        <label>
+            área:
+            <select ng-model="data._tipo1" ng-change="set(1)">
+                <option ng-repeat="(key, val) in data._areas" ng-value="key">{{key}}</option>
+            </select>
+        </label>
+        <label ng-show="data._tipo1">
+            segmento:
+            <select ng-model="data._tipo2">
+                <option ng-repeat="val in data._segmentos" ng-value="val">{{val}}</option>
+            </select>
+        </label>
+    </edit-box>
+</div>
