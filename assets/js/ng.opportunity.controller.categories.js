b3962c07fa8ca418bca18f24c8936352dfc1e30c
Gabriel Avaloni <avaloni@users.noreply.github.com>
2017-12-19 15:58:39 -0200

    Adição de tipologia de categoria na inscrição de forma multipla

--- assets/js/ng.opportunity.controller.categories.js
+++ assets/js/ng.opportunity.controller.categories.js
@@ -5,68 +5,43 @@
     
     module.controller('CategoriesController',['$scope', 'EditBox', function($scope, EditBox){
         $scope.editBox = EditBox;
-        // console.log(MapasCulturais.segmentos);
-        
-        $scope.segmentos = MapasCulturais.segmentos;
-        
-
-        // var n1 = MapasCulturais.entity.tipologia_nivel1;
-        // var n2 = MapasCulturais.entity.tipologia_nivel2;
-        // var n3 = MapasCulturais.entity.tipologia_nivel3;
-
-        // types.__values = Object.keys(types);
-        // types.__values.forEach(function(val){
-        //     types[val].__values = Object.keys(types[val]);
-        // });
-        
-
-        $scope.data = {
-            // _tipo1: [],
-            // _tipo2: [],
-            // _tipo3: n3,
-            
-            // tipologia1: n1,
-            // tipologia2: n2,
-            // tipologia3: n3,
-            
-            // _types: types,
-            // _valores_nivel1: types.__values,
-            // _valores_nivel2: n1 ? types[n1].__values : [],
-            // _valores_nivel3: n2 ? types[n1][n2] : [],
-            _areas: $scope.segmentos,
-            _segmentos: []
-        };
-        
-
-        
-        $scope.set = function(n){
-            if(n === 1){
-                $scope.data._segmentos = $scope.data._areas[$scope.data._tipo1];
-                
-                $scope.data._tipo2 = '';
+        $scope.tipologias = MapasCulturais.segmentos;
+        $scope.tipologiaAtuacao = [
+            {
+                id: 0,
+                _areas: $scope.tipologias,
+                _segmentos: []
             }
+        ];
+
+        $scope.adicionarSegmento = function() {
+            var novoSegmento = $scope.tipologiaAtuacao.length+1;
+            $scope.tipologiaAtuacao.push(
+                {
+                    'id': novoSegmento,
+                    _areas: $scope.tipologias,
+                    _segmentos: []
+                }
+            );
         };
-        
-        var setEditables = function(){
-            $('#area').first().editable('setValue', $scope.data.area);
-            $('#segmento').first().editable('setValue', $scope.data.segmento);
+
+        $scope.removerSegmento = function() {
+            var ultimoSegmento = $scope.tipologiaAtuacao.length-1;
+            $scope.tipologiaAtuacao.splice(ultimoSegmento);
         };
-        
-        setEditables();
+
+
+        $scope.set = function(index){
+            $scope.tipologiaAtuacao[index]._segmentos = $scope.tipologias[$scope.tipologiaAtuacao[index].area];
+            $scope.data._tipo2 = '';
+        };
+
         
         $scope.setTypes = function(){
-            $scope.data.area = $scope.data._tipo1;
-            $scope.data.segmento = $scope.data._tipo2;
-            
-            setEditables();
-            
-            EditBox.close('eb-tipologia');
-        };
-        
-        
-        $scope.resetValues = function(){
-            $scope.data._tipo1 = $scope.data.area;
-            $scope.data._tipo2 = $scope.data.segmento;
+            var $box = jQuery('[id^=eb-tipologia]').find('>div.edit-box');
+            $box.hide();
+
+            jQuery('[id^=eb-tipologia]').trigger('close');
         };
         
     }]);
--- layouts/parts/singles/registration-edit--categories.php
+++ layouts/parts/singles/registration-edit--categories.php
@@ -1,34 +1,25 @@
-
-<!--<?php if($opportunity->registrationCategories): ?>
-    <div class="registration-fieldset">
-        <h4><?php echo $opportunity->registrationCategTitle ?></h4>
-        <p class="registration-help"><?php echo $opportunity->registrationCategDescription ?></p>
-        <p>
-            <span class='js-editable-registrationCategory' data-original-title="<?php \MapasCulturais\i::esc_attr_e("Opção");?>" data-emptytext="<?php \MapasCulturais\i::esc_attr_e("Selecione uma opção");?>" data-value="<?php echo htmlentities($entity->category) ?>"><?php echo $entity->category ?></span>
-        </p>
-    </div>
-<?php endif; ?>
--->
-
-
 <div class="registration-fieldset" ng-controller="CategoriesController">
     <h4>Tipologia de categoria</h4>
-    <a class="required editable" ng-click="editBox.open('eb-tipologia', $event)"> {{data.segmento ? data.segmento : 'Escolha um segmento'}}</a>
+    <div ng-repeat="tipologia in tipologiaAtuacao">
+        <a class="required editable" ng-click="editBox.open('eb-tipologia'+$index, $event)"> {{tipologia.segmento ? tipologia.segmento : 'Escolha um segmento'}}</a>
 
-    <edit-box id="eb-tipologia" position="bottom" cancel-label="Cancelar" submit-label="Enviar" on-submit="setTypes" on-cancel="resetValues" close-on-cancel="1">
-        <input type="hidden" id="data.area" class="js-editable" data-edit="data.area" data-emptytext="">
-        <input type="hidden" id="data.segmento" class="js-editable" data-edit="data.segmento" data-emptytext="">
-        <label>
-            área:
-            <select ng-model="data._tipo1" ng-change="set(1)">
-                <option ng-repeat="(key, val) in data._areas" ng-value="key">{{key}}</option>
-            </select>
-        </label>
-        <label ng-show="data._tipo1">
-            segmento:
-            <select ng-model="data._tipo2">
-                <option ng-repeat="val in data._segmentos" ng-value="val">{{val}}</option>
-            </select>
-        </label>
-    </edit-box>
+        <edit-box  id="eb-tipologia{{$index}}"position="bottom" cancel-label="Cancelar" submit-label="Enviar" on-submit="setTypes">
+            <label>
+                área:
+                <select ng-model="tipologia.area" ng-change="set($index)">
+                    <option ng-repeat="(key, val) in tipologia._areas" ng-value="key">{{key}}</option>
+                </select>
+            </label>
+            <label ng-show="tipologia.area">
+                segmento:
+                <select ng-model="tipologia.segmento">
+                    <option ng-repeat="val in tipologia._segmentos" ng-value="val">{{val}}</option>
+                </select>
+            </label>
+
+        </edit-box>
+
+    </div>
+    <a class="btn btn-danger delete" ng-click="removerSegmento()">Remover</a>
+    <a class="btn btn-default add" ng-click="adicionarSegmento()">Novo Segmento</a>
 </div>
