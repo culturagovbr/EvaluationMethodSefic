4b579eb91c8bc9dbd7194bd99fe7f9a597cfb6b0
Gabriel Avaloni <avaloni@users.noreply.github.com>
2017-12-19 19:53:38 -0200

    Correção de template, para as views só rodarem nas oportunidades com o plugin

--- layouts/parts/singles/opportunity-registrations--categories-sefic.php
+++ layouts/parts/singles/opportunity-registrations--categories-sefic.php
@@ -7,7 +7,7 @@ $editable_class = $can_edit ? 'js-editable' : '';
 <div id="registration-categories" class="registration-fieldset" ng-controller="CategoriesController">
     <h4>Tipologia de atuação</h4>
     <p class="registration-help" >É possível selecionar multiplas áreas e segmentos.</p>
-    <div ng-repeat="(k,s) in segmentos">
+    <div ng-repeat="(k,s) in tipologias">
         <p>
             <h5><?php \MapasCulturais\i::_e("Área");?></h4>
             {{k}}
