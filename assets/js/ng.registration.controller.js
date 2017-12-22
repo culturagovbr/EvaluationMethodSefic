9f6dac64e759cdf3b0d6ac06825f39e9355bacde
Pedro Philipe Alves de Oliveira <pedro.oliveira2@cultura.gov.br>
2017-12-22 19:02:15 -0200

    Salvando diversas inscrições por segmento da categoria

--- assets/js/ng.registration.controller.js
+++ assets/js/ng.registration.controller.js
@@ -14,24 +14,36 @@
             }
         }
 
-        console.log(MapasCulturais);
-
         $scope.createOpportunityRegistration = function() {
             // it works
             var registration = {};
-            angular.forEach(jQuery('[class*="js-editable"]'), function(e, i){
-                registration[jQuery(e)[0].id] = jQuery(e)[0].textContent;
+            registration['category'] = [];
+
+            angular.forEach(jQuery('[class*="js-editable"]'), function(e){
+                if(jQuery(e)[0].id == "category"){
+                    registration['category'].push(jQuery(e)[0].textContent);
+                }else{
+                    registration[jQuery(e)[0].id] = jQuery(e)[0].textContent;
+                }
             });
+
             registration['ownerId'] = MapasCulturais.entity.ownerId;
             registration['opportunityId'] = MapasCulturais.entity.object.opportunity.id;
 
-            // MapasCulturais.createUrl(MapasCulturais.request.controller, 'edit', MapasCulturais.request.id)
 
-            $http.post('/inscricoes/'+MapasCulturais.request.id, registration).success(function (data, status) {
-                MapasCulturais.Messages.success(labels['changesSaved']);
-            }).error(function (data, status) {
+            //here be dragons
+            angular.forEach(registration['category'],function(v){
+                registration.category = v;
+                var data = Object.assign({}, registration);
+
+                $http.post('/inscricoes/', data).success(function (data, status) {
+                    MapasCulturais.Messages.success(labels['changesSaved']);
+                }).error(function (data, status) {
+                    MapasCulturais.Messages.error(labels['correctErrors']);
+                });
 
             });
+
         };
 
     }]);
