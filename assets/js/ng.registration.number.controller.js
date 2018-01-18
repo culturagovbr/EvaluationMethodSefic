2f7f1f3d3fc3006e560e380059db7af4fb0b18de
Pedro Philipe Alves de Oliveira <pedrophilipeti@gmail.com>
2018-01-18 17:27:42 -0200

    Corrige Url da registration

--- assets/js/ng.registration.number.controller.js
+++ assets/js/ng.registration.number.controller.js
@@ -13,7 +13,7 @@
         $scope.$watch('data.registrations', function(){
             angular.forEach($scope.data.registrations, (e) => {
                 e.number = "on-" + e.previousPhaseRegistrationId;
-                e.singleUrl = e.singleUrl.replace(e.id, e.previousPhaseRegistrationId);
+                // e.singleUrl = e.singleUrl.replace(e.id, e.previousPhaseRegistrationId);
             });
         });
     }]);
